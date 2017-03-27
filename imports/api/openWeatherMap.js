4326eed9c035d73c766fba229a1d5b6dee4ef67b
spixooze <s_simon_s@hotmail.com>
2017-03-27 17:22:58 +0200

    Add crossorigin for HTTPS

--- imports/api/openWeatherMap.js
+++ imports/api/openWeatherMap.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //URL
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/forecast?';
+const OPEN_WEATHER_MAP_URL = 'https://crossorigin.me/http://api.openweathermap.org/data/2.5/forecast?';
 const FORMAT = 'metric';
 const APIKEY = 'e6d4d600af44fb788e52102d2c5198b0';
 
@@ -10,7 +10,7 @@ export default getWeather = (location) => {
 	let requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}&units=${FORMAT}&appid=${APIKEY}&mode=json`;
 	//temp
 	console.log(requestUrl);
-	
+
 	return axios.get(requestUrl).then((res) => {
 		if (res.data.cod !== '200' && res.data.message) {
 			throw new Error(res.data.message);
